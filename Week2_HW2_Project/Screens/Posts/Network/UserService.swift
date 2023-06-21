11c433a80439886f2f60afc3d4ac709eea6aae43
Eric <eryoung610@gmail.com>
2023-06-21 12:58:30 -0700

    Created UserService

--- Week2_HW2_Project/Screens/Posts/Network/UserService.swift
+++ Week2_HW2_Project/Screens/Posts/Network/UserService.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Combine
+import SwiftUI
+
+class UserService {
+    var cancellable = Set<AnyCancellable>()
+    let urlString = "https://jsonplaceholder.typicode.com/posts"
+    
+    func fetchUsers() -> Future<[User], Error> {
+        
+        return Future { promise in
+            let finalUrlString = self.urlString
+            guard let url = URL(string : finalUrlString) else {return}
+            
+            URLSession.shared.dataTaskPublisher(for: url)
+                .map{$0.data}
+                .decode(type:[User].self, decoder:JSONDecoder())
+                .receive(on: RunLoop.main)
+                .sink { completion in
+                    switch completion {
+                    case .finished:
+                        break
+                    case .failure(let err) :
+                        promise(.failure(err))
+                    }
+                } receiveValue : { users in
+                    promise(.success(users))
+//                    print("Received Users: \(response.user)")
+                }
+                .store(in : &self.cancellable)
+        }
+    }
+}
