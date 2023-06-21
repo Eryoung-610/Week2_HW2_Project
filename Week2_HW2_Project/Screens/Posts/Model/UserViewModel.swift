a5711020d9927f58988d853ee0759a379c3700d1
Eric <eryoung610@gmail.com>
2023-06-21 12:48:34 -0700

    Initial File Structure Created

---
+++ Week2_HW2_Project/Screens/Posts/Model/UserViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserViewModel.swift
+//  Week2_HW2_Project
+//
+//  Created by Eric Young on 6/21/23.
+//
+
+import Foundation
+
---
+++ Week2_HW2_Project/Screens/Posts/Network/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  Week2_HW2_Project
+//
+//  Created by Eric Young on 6/21/23.
+//
+
+import Foundation
