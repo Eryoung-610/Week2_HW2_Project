4b89acdd6b8be404408d4d7f11abcc568c1b3dca
Eric <eryoung610@gmail.com>
2023-06-21 12:59:53 -0700

    Update UserViewModel.swift

--- Week2_HW2_Project/Screens/Posts/Model/UserViewModel.swift
+++ Week2_HW2_Project/Screens/Posts/Model/UserViewModel.swift
@@ -6,4 +6,27 @@
 //
 
 import Foundation
+import Combine
+import SwiftUI
 
+class UserViewModel : ObservableObject {
+    @Published var users = [User]()
+    
+    var cancellable = Set<AnyCancellable>()
+    let service = UserService()
+    
+    func getUsers() {
+        service.fetchUsers()
+            .sink { completion in
+                switch completion {
+                case .finished :
+                    break
+                case .failure(let err) :
+                    print(err.localizedDescription)
+                }
+            } receiveValue: { users in
+                self.users = users
+            }
+            .store(in: &cancellable)
+    }
+}
