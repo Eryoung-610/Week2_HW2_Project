24e99141186cf9dcb539c1f792628b8da68f989a
Eric <eryoung610@gmail.com>
2023-06-21 12:34:14 -0700

    Initial Commit

---
+++ Week2_HW2_Project/Week2_HW2_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week2_HW2_ProjectApp.swift
+//  Week2_HW2_Project
+//
+//  Created by Eric Young on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Week2_HW2_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
